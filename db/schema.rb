73c9873ec3e401f2786dd65be46e898bc203156c
Ian <heratyian@gmail.com>
2023-05-01 19:06:46 +0000

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_190342) do
+ActiveRecord::Schema.define(version: 2023_05_01_190553) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
