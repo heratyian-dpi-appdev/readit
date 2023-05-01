ac55503a6ce84cb6207a80047423f42c480d3607
Ian <heratyian@gmail.com>
2023-05-01 19:09:24 +0000

    Add associations to user, post, and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,6 @@
 #  user_id    :integer
 #
 class Comment < ApplicationRecord
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
+  belongs_to(:post, { :required => true, :class_name => "Post", :foreign_key => "post_id" })
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,6 @@
 #  user_id    :integer
 #
 class Post < ApplicationRecord
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "post_id", :dependent => :destroy })
 end
